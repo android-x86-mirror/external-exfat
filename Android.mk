744e8efa055f0bdfbe888beffdd9448e6326af8a
Chih-Wei Huang <cwhuang@linux.org.tw>
2016-07-07 01:17:37 +0800

    Clean up Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,1 @@
-FUSE_ROOT := $(call my-dir)
-
-include $(FUSE_ROOT)/libexfat/Android.mk
-include $(FUSE_ROOT)/fuse/Android.mk
-include $(FUSE_ROOT)/mkfs/Android.mk
-include $(FUSE_ROOT)/fsck/Android.mk
+include $(call all-subdir-makefiles)
